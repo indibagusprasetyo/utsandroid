9286664020539d0d18bb4d0b375e31d7d25bf7cf
indibagusprasetyo <endang.saefulloh@gmail.com>
2024-06-06 17:20:36 +0700

    Coding baru
    Upload Baru

---
+++ app/src/main/java/com/example/uts_praktikum_android/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.uts_praktikum_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// Nama         : Indi Bagus Prasetyo
+// NIM          : 10121902
+// Kelas        : IF9K
+// Jurusan      : Teknik Informatika
+// Mata Kuliah  : Pemrograman Android
+// Dosen        : Rizki Adam Kurniawan
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uts_praktikum_android/MainActivity3.kt
@@ -0,0 +1,28 @@
+package com.example.uts_praktikum_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// Nama         : Indi Bagus Prasetyo
+// NIM          : 10121902
+// Kelas        : IF9K
+// Jurusan      : Teknik Informatika
+// Mata Kuliah  : Pemrograman Android
+// Dosen        : Rizki Adam Kurniawan
+
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
